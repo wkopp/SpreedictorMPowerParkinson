3dc6f4a25fc06a357ebc67d3893637e1e313fdb1
glichtner <gregor.lichtner@charite.de>
2017-07-12 16:36:12 +0200

    added timeseries length analysis

--- code/walkingactivity.py
+++ code/walkingactivity.py
@@ -4,10 +4,16 @@ import numpy as np
 import os
 import joblib
 import itertools
+import quaternion
 
 datadir = os.getenv('PARKINSON_DREAM_DATA')
 
 class WalkingActivity(object):
+    modalities = ['deviceMotion', 'accel', 'pedometer']
+    variants = ['outbound', 'rest', 'return']
+    modality_variants = sorted(list(set([x for x in itertools.product(modalities, variants)]) - set([('pedometer', 'rest')])))
+    modality_variants_timeseries = sorted([x for x in itertools.product(['deviceMotion', 'accel'], ['outbound', 'rest', 'return'])])
+
     def __init__(self, limit = None, download_jsons = True, reload_ = False):
 
         self.synapselocation = 'syn10146553'
@@ -113,7 +119,7 @@ class WalkingActivity(object):
         fid = dataEntry[colname]
 
         if pd.isnull(fid[0]):
-            return pd.nan
+            return pd.DataFrame()
 
         fid = str(int(fid))
         if fid not in self.file_map:
@@ -126,13 +132,69 @@ class WalkingActivity(object):
 
         content['healthCode'] = dataEntry["healthCode"][0]
         content['recordId'] = recordId
-        content['time_in_task'] = content['timestamp'] - content['timestamp'][0]
+        if not modality == 'pedometer':
+            content['time_in_task'] = content['timestamp'] - content['timestamp'][0]
 
         return content
+
 
     def getEntryByIndex(self, idx, modality, variant):
         return self.getEntryByRecordId(self.commondescr.iloc[idx]['recordId'], modality, variant)
 
+
+    def convertUserAccelerationToWorldFrame(self, ts):
+        colNames = wa.getColNames('userAcceleration_world')
+
+        if ts.columns.intersection(colNames).size == len(colNames):
+            return ts
+
+        rot = np.empty((ts.shape[0], 3))
+        rot[:] = np.NAN
+        for i in range(ts.shape[0]):
+                rot[i, :] = quaternion.quat_mult(ts[['attitude_x', 'attitude_y', 'attitude_z', 'attitude_w']].iloc[i],
+                                     ts[['userAcceleration_x', 'userAcceleration_y', 'userAcceleration_z']].iloc[i])
+
+        ts[colNames] = pd.DataFrame(rot, index=ts.index)
+
+        return ts
+
+    def extractTimeseriesLengths(self, limit = None, reload_ = False):
+        cache_filename_ts_lengths = 'ts_lengths.pkl' if not limit else 'ts_lengths_{:d}.pkl'.format(
+            limit)
+        cachepath_ts_lengths = os.path.join(self.downloadpath, cache_filename_ts_lengths)
+
+        if os.path.exists(cachepath_ts_lengths) and not reload_:
+            df_ts_lengths = joblib.load(cachepath_ts_lengths)
+        else:
+            n_modality_variants = len(self.modality_variants_timeseries)
+            n_timeseries = self.commondescr.shape[0]
+            ts_lengths = np.empty((n_timeseries, n_modality_variants * 2))
+            ts_lengths[:] = np.nan
+
+            for i_col, (modality, variant) in enumerate(self.modality_variants_timeseries):
+                print modality, variant
+
+                for i_row in range(n_timeseries):
+                    ts = self.getEntryByIndex(i_row, modality=modality, variant=variant)
+                    if ts.size > 0:
+                        ts_lengths[i_row, i_col] = ts.shape[0]
+                        ts_lengths[i_row, i_col + n_modality_variants] = ts['timestamp'][1] - ts['timestamp'][0]
+
+            df_ts_lengths = pd.DataFrame(
+                data=ts_lengths,
+                columns=['_'.join(x) for x in self.modality_variants_timeseries] +
+                        ['_'.join(x + ('timestep',)) for x in self.modality_variants_timeseries]
+            )
+            df_ts_lengths[['recordId', 'healthCode']] = self.getCommonDescriptor()[
+                ['recordId', 'healthCode']].reset_index(drop=True)
+
+            joblib.dump(df_ts_lengths, cachepath_ts_lengths)
+
+        return df_ts_lengths
+
 if __name__ == '__main__':
-    wa = WalkingActivity(limit = 1000, download_jsons = True)
-    wa.getEntryByIndex(0, modality='deviceMotion', variant='outbound')
+    wa = WalkingActivity(limit = 3000, download_jsons = True)
+    #ts = wa.getEntryByIndex(0, modality='pedometer', variant='outbound')
+    #wa.convertUserAccelerationToWorldFrame(ts)
+    #print wa.modality_variants
+    wa.extractTimeseriesLengths(limit=1000, reload_=True)
