1d352521a81e59b1a0cc74389c2d1615d7a5fa55
glichtner <gregor.lichtner@charite.de>
2017-07-19 23:38:32 +0200

    added quaternion rotation and "single axis feature" extraction

--- code/walkingactivity.py
+++ code/walkingactivity.py
@@ -179,8 +179,8 @@ class WalkingActivity(object):
         return df_ts_lengths
 
 if __name__ == '__main__':
-    wa = WalkingActivity(limit = 10000, download_jsons = True)
+    wa = WalkingActivity(limit = None, download_jsons = True)
     #ts = wa.getEntryByIndex(0, modality='pedometer', variant='outbound')
     #wa.convertUserAccelerationToWorldFrame(ts)
     #print wa.modality_variants
-    wa.extractTimeseriesLengths(limit=1000)
+    #wa.extractTimeseriesLengths(limit=1000)
